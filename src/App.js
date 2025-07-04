acd85766499091529d4cff2d17d2028255b9ed3a
elena-miles <elena.airey@gmail.com>
2025-07-04 10:51:58 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';  
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <div className="container">
     <h1>Weather App</h1>
     <footer className="App-footer">
     This project is coded by <a href='' target='_blank'>Elena Miles</a> and is open-sourced on{" "}<a href='https://github.com/elena-miles/react-weather-app-em' target='_blank'>Github</a> and hosted on {" "}<a href='https://app.netlify.com/projects/react-weather-app-elena' target='_blank'>Netlify</a>.
     </footer>
+    </div>
     </div>
   );
 }
