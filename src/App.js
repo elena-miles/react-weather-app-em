928da0a43fb214e44d3c93fcd6627b632be883df
elena-miles <elena.airey@gmail.com>
2025-07-04 10:31:00 +0200

    added git repo URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
     <h1>Weather App</h1>
+    <footer className="App-footer">
+    This project is coded by <a href='' target='_blank'>Elena Miles</a> and is open-sourced on{" "}<a href='https://github.com/elena-miles/react-weather-app-em' target='_blank'>Github</a> and hosted on {" "}<a href='https://app.netlify.com/projects/react-weather-app-elena' target='_blank'>Netlify</a>.
+    </footer>
     </div>
   );
 }
