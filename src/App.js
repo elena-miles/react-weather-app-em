ffb5d33afc4b0ba193b61f6567822eaef389dd7f
elena-miles <elena.airey@gmail.com>
2025-07-07 13:54:11 +0200

    CSS to match the google app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import './App.css';
+import Weather from './Weather.js';
+import './Weather.css';
 
 function App() {
   return (
     <div className="App">
-    <div className="container">
-    <h1>Weather App</h1>
-    <footer className="App-footer">
-    This project is coded by <a href='' target='_blank'>Elena Miles</a> and is open-sourced on{" "}<a href='https://github.com/elena-miles/react-weather-app-em' target='_blank'>Github</a> and hosted on {" "}<a href='https://app.netlify.com/projects/react-weather-app-elena' target='_blank'>Netlify</a>.
-    </footer>
-    </div>
+      <div className="container">
+        <Weather/>
+
+        <footer className="App-footer">
+        This project is coded by <a href='https://github.com/elena-miles' target='_blank' rel="noreferrer">Elena Miles</a> and is open-sourced on{" "}<a href='https://github.com/elena-miles/react-weather-app-em' target='_blank' rel="noreferrer">Github</a> and hosted on {" "}<a href='https://app.netlify.com/projects/react-weather-app-elena' target='_blank' rel="noreferrer">Netlify</a>.
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function Weather() { 
+    return (
+        <div className="Weather">
+
+        <form className="form mx-auto mb-2" style={{ maxWidth: "450px" }}>
+            <div className="row">
+                <div className="col-9">
+                <input type="search" className="form-control" placeholder="Enter a city..." autoFocus="on" />
+                </div>
+                <div className="col-3">
+                <button type="submit" className="btn btn-primary col-3 w-100" value="Search">Search</button>
+               </div>
+            </div>
+        </form>
+
+          <h1 className="City">City</h1>
+
+          <ul className="Weather-details">
+            <li><span className="Day">Day</span>,{" "}<span className="Time">Time</span></li>
+            <li className="Condition">Condition</li>
+          </ul>
+        
+            <div className="row">
+                <div className="col-6 weather-icon-display">
+                <div className="d-flex align-items-center">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Weather Icon" className="me-1" />
+                <span className="temperature">6</span><span className="temperature-unit">°C</span>
+                </div>
+            </div>
+
+            <div className="col-6">
+                    <ul>
+                        <li>Precipitation: 0%</li>
+                        <li>Humidity: 0%</li>
+                        <li>Wind: 0 km/h</li>
+                    </ul>
+            </div>
+            </div>
+        </div>
+); 
+}
