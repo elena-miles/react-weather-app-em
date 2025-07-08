43ba2fc8ce4af3932437fe6953ce2527ead2fac0
elena-miles <elena.airey@gmail.com>
2025-07-08 12:22:43 +0200

    axios and api key

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import axios from "axios";
+
 
 export default function Weather() { 
+    // State variables to hold weather data 
+    const [Temperature, setTemperature] = useState(null);
+    const [Loading, setLoading] = useState(false);
+
+
+    function handleResponse(response) {
+        console.log(response.data);
+        setTemperature(response.data.main.temp);
+        setLoading(true);
+    }
+
+    const APIkey="ac209dae1f283fb332a5bb7f50b0f468";
+    const [city, setCity] = useState("London"); // Default city
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
     return (
         <div className="Weather">
 
@@ -24,18 +42,18 @@ export default function Weather() {
           </ul>
         
             <div className="row">
-                <div className="col-12 col-md-6 weather-icon-display">
+                <div className="col-12 col-sm-6 weather-icon-display">
                 <div className="d-flex">
                 <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Weather Icon" className="me-1" />
-                <span className="temperature">6</span><span className="temperature-unit mt-3">°C</span>
+                <span className="temperature">{Math.round(temperature)}</span><span className="temperature-unit mt-3">°C</span>
                 </div>
             </div>
 
-            <div className="col-12 col-md-6">
+            <div className="col-12 col-sm-6">
                     <ul>
-                        <li>Precipitation: 0%</li>
-                        <li>Humidity: 0%</li>
-                        <li>Wind: 0 km/h</li>
+                        <li>Precipitation:{" "}{}%</li>
+                        <li>Humidity:{" "}{}%</li>
+                        <li>Wind:{" "}{}km/h</li>
                     </ul>
             </div>
             </div>
